77aeffed3d98e8127ee232b0b8353058886e72ea
RamanBhattarai <ramannr11@gmail.com>
2023-07-14 17:52:48 +0545

    Updated

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const {Pool} = require ('pg')
 const express = require ('express');
 const app = express();
-const cors = require('cors');
+//const cors = require('cors');
 require('dotenv').config();
 
-app.use(cors());
+//app.use(cors());
 
 app.use(express.json());
 
